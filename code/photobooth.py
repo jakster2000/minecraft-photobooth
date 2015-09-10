1c6a67cf6e3840fec7622e63f24fc5a1df95afd8
jakster2000 <jakub2000.10@hotmail.co.uk>
2015-09-10 14:06:44 +0100

    Update photobooth.py

--- code/photobooth.py
+++ code/photobooth.py
@@ -19,15 +19,5 @@ def where_am_i():
         if x >= 10.5 and y == 9.0 and z == -44.3:
         #print "You are at the photobooth!"
         
-            mc.postToChat("You are in the Photobooth!")
-            time.sleep(1)
-            mc.postToChat("Smile!")
-            time.sleep(1)
-            take_the_pic()
-            mc.postToChat("Check out your picture")
-            time.sleep(5)
-        
-mc.postToChat("Find the Photo-Booth")
-where_am_I()
-    
-
+            mc.postToChat("Out Of Service!")
+            
